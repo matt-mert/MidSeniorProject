712c52891d54b1c32bfe274fe348ba5f11f55e62
matt-mert <mertkucukakinci@gmail.com>
2022-08-12 01:28:36 +0300

    Snake improvements continued

--- Assets/Challenges/2. ModifiedSnake/Scripts/MonoBehaviours/GameVisualizer.cs
+++ Assets/Challenges/2. ModifiedSnake/Scripts/MonoBehaviours/GameVisualizer.cs
@@ -14,20 +14,25 @@ namespace Challenges._2._ModifiedSnake.Scripts.MonoBehaviours
         [Inject]
         private IMap _map;
 
-
         private Transform Spawn()
         {
             var ins = Instantiate(mapLinePrefab, transform);
             return ins;
         }
+
         private void Start()
         {
             ground.localScale = new Vector3(_map.MapSize.x+1f,1f,_map.MapSize.y+1f);
+            var groundRenderer = ground.GetComponent<MeshRenderer>();
+            groundRenderer.material.mainTextureScale = new Vector2(_map.MapSize.x / 2f, _map.MapSize.y / 2f);
+
             if (_map == null) return;
+
             var center = (_map.ToWorldPosition(new Vector2Int(0, 0)) +
                              _map.ToWorldPosition(new Vector2Int(_map.MapSize.x - 1, _map.MapSize.y - 1))) / 2f;
             var mapSizeX = _map.MapSize.x;
             var mapSizeY = _map.MapSize.y;
+
             for (int x = 0; x < _map.MapSize.x+1; x++)
             {
                 var line = Spawn();
@@ -38,6 +43,7 @@ namespace Challenges._2._ModifiedSnake.Scripts.MonoBehaviours
                 line.position = worldPos;
                 line.localScale = new Vector3(0.05f, 0.05f, mapSizeY);
             }
+
             for (int y = 0; y < _map.MapSize.y+1; y++)
             {
                 var line = Spawn();
@@ -49,7 +55,5 @@ namespace Challenges._2._ModifiedSnake.Scripts.MonoBehaviours
                 line.localScale = new Vector3(0.05f, 0.05f, mapSizeX);
             }
         }
-
-     
     }
 }
