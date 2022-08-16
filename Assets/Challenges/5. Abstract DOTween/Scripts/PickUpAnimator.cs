58d598d8bdd64b625e5e57cd729a345a81c99020
matt-mert <mertkucukakinci@gmail.com>
2022-08-17 00:07:19 +0300

    DOTween challenge continued

--- Assets/Challenges/5. Abstract DOTween/Scripts/PickUpAnimator.cs
+++ Assets/Challenges/5. Abstract DOTween/Scripts/PickUpAnimator.cs
@@ -1,14 +1,24 @@
-﻿using DG.Tweening;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
 
 namespace Challenges._6._Abstract_DOTween.Scripts
 {
-    
     public class PickUpAnimator : DoTweenAnimation
     {
-        //Add parameters here
+        // Note: Loops inside animation sequences can also be implemented,
+        // a set of animations in a sequence can be set loopable then.
+        // I did not add loops as it was not a requirement.
+        
+        public enum Actions
+        {
+            MoveObject,
+            ScaleObject
+        }
 
-        public float startingHeight;
-
+        [Header("Complete sequence of the animation")]
+        public List<ActionList> actionSequence;
         
         /// <summary>
         /// Fill out this function
@@ -17,7 +27,82 @@ namespace Challenges._6._Abstract_DOTween.Scripts
         public override Tween StartPreview()
         {
             var sequence = DOTween.Sequence();
+
+            if (actionSequence.Count == 0) return null;
+
+            for (int i = 0; i < actionSequence.Count; i++)
+            {
+                sequence.AppendInterval(actionSequence[i].delayBeforeSet);
+                var actions = actionSequence[i].setOfActions;
+
+                if (actions.Count == 0) continue;
+
+                for (int j = 0; j < actions.Count; j++)
+                {
+                    var actionType = actions[j].actionType;
+                    var objs = actions[j].animatedObjects;
+                    var easeType = actions[j].actionEaseType;
+                    var snapping = actions[j].isSnapping;
+                    var direction = actions[j].actionDirection;
+                    var duration = actions[j].actionDuration;
+
+                    if (j == 0)
+                    {
+                        switch (actionType)
+                        {
+                            case Actions.MoveObject:
+                                foreach (var obj in objs) sequence.Append(obj.DOMove(direction, duration, snapping).SetEase(easeType));
+                                break;
+                            case Actions.ScaleObject:
+                                foreach (var obj in objs) sequence.Append(obj.DOScale(direction, duration).SetEase(easeType));
+                                break;
+                            default:
+                                throw new ArgumentOutOfRangeException(nameof(actionType), actionType, null);
+                        }
+                        continue;
+                    }
+
+                    switch (actionType)
+                    {
+                        case Actions.MoveObject:
+                            foreach (var obj in objs) sequence.Join(obj.DOMove(direction, duration, snapping).SetEase(easeType));
+                            break;
+                        case Actions.ScaleObject:
+                            foreach (var obj in objs) sequence.Join(obj.DOScale(direction, duration).SetEase(easeType));
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException(nameof(actionType), actionType, null);
+                    }
+                }
+
+                sequence.AppendInterval(actionSequence[i].delayAfterSet);
+            }
+            
             return sequence;
         }
+    }
+
+    [Serializable]
+    public class ActionList
+    {
+        [Header("A set of actions will be executed simultaneously")]
+        public float delayBeforeSet = 0;
+        public List<Action> setOfActions;
+        public float delayAfterSet = 0;
+    }
+
+    // Note: Tooltips can be added for each parameter.
+
+    [Serializable]
+    public class Action
+    {
+        [Header("(Not all properties are applicable for all actions)")]
+        [Header("Choose the properties of this action")]
+        public PickUpAnimator.Actions actionType;
+        public Transform[] animatedObjects;
+        public Ease actionEaseType = Ease.OutQuad;
+        public bool isSnapping = false;
+        public Vector3 actionDirection = Vector3.zero;
+        public float actionDuration = 0;
     }
 }
